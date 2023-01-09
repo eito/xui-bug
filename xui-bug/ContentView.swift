55f9c490dcad09503b7f85fd5b3feec118957b50
Eric Ito <eric.ito@gmail.com>
2023-01-09 14:48:24 -0800

    update

--- xui-bug/ContentView.swift
+++ xui-bug/ContentView.swift
@@ -6,8 +6,62 @@
 //
 
 import SwiftUI
+import XUI
 
+protocol OtherViewModelItemProtocol {
+    var name: String { get }
+}
+
+protocol OtherViewModelProtocol: ViewModel, ObservableObject, Identifiable {
+
+    var otherText: String { get }
+
+    var items: [ItemType] { get }
+
+    associatedtype ItemType: OtherViewModelItemProtocol
+}
+
+protocol MainViewModelProtocol: ViewModel {
+
+    var text: String { get }
+
+    var otherViewModel: OtherViewModelType? { get set }
+
+    associatedtype OtherViewModelType: OtherViewModelProtocol
+}
+
+class OtherViewModelItem: OtherViewModelItemProtocol {
+    let name = "Other View Model Item"
+}
+
+class OtherViewModel: OtherViewModelProtocol {
+
+    let id = UUID()
+    let otherText = "Other View model"
+
+    let items = [OtherViewModelItem()]
+
+    init() {}
+}
+
+class MainViewModel: MainViewModelProtocol, ObservableObject {
+
+    let text = "Hello from MainViewModel"
+
+    @Published var otherViewModel: OtherViewModel?
+
+    init() {
+
+    }
+}
+
+// Fails to compile
+//
 struct ContentView: View {
+
+    @Store
+    var viewModel: any MainViewModelProtocol
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,11 +70,47 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .sheet(item: $viewModel.otherViewModel) { viewModel in
+            Text(viewModel.otherText)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+// Compiles
+//
+//struct ContentView: View {
+//
+//    @Store
+//    var viewModel: any MainViewModelProtocol
+//
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+
+// Compiles -- remove ContentView(viewModel: MainViewModel()) and replace with ContentView()
+//
+//struct ContentView: View {
+//
+//    @Store
+//    var viewModel = MainViewModel()
+//
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//        .sheet(item: $viewModel.otherViewModel) { viewModel in
+//            Text(viewModel.otherText)
+//        }
+//    }
+//}
