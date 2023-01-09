8e87c59f6e6f443e2c9ca297ee66aeefd4529d29
Eric Ito <eric.ito@gmail.com>
2023-01-09 14:50:30 -0800

    Initial Commit

---
+++ xui-bug/xui_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  xui_bugApp.swift
+//  xui-bug
+//
+//  Created by Eric Ito on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct xui_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
